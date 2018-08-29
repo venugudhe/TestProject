136a49486507ee4dae761d028e77404fb0c67b31
prasanthgudhe <prasanthgudhe9@gmail.com>
2018-08-29 17:05:28 +0530

    i have wrote code in employee class so plz go and check it

--- TestProject/src/com/rajasri/employee/Employee.java
+++ TestProject/src/com/rajasri/employee/Employee.java
@@ -1,5 +1,16 @@
 package com.rajasri.employee;
 
 public class Employee {
-
+	public int i1=10;
+	public static int i2=20;
+   public static void main(String[] args) {
+	
+	Employee e1=new Employee();
+	e1.i1=20;
+	e1.i2=30;
+	Employee e2=new Employee();
+	e2.i1=30;
+	e2.i2=40;
+	System.out.println(e1.i1+","+e1.i2+","+e2.i1+","+e2.i2);
+}
 }
